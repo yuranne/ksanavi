5023a5e98a8f1ad0f8f718b85fd01202a5007c6e
yuranne <22-113@ksa.hs.kr>
2024-05-27 23:07:25 +0900

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,6 +11,7 @@ export default defineConfig({
         home: resolve(__dirname, 'ko/home.html'),
         schedule: resolve(__dirname, 'ko/schedule.html'),
         navigation: resolve(__dirname, 'ko/navigation.html'),
+        navigation_js: resolve(__dirname, 'ko/navigation.js'),
         campus_map: resolve(__dirname, 'ko/campus_map.html'),
         schedule_setting: resolve(__dirname, 'ko/schedule_setting.html'),
         home_en: resolve(__dirname, 'en/home.html'),
