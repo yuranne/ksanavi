76b5a1109751495225d82227723012365a289928
qhflrjsQkd <23-010@ksa.hs.kr>
2024-05-27 20:06:30 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import * as THREE from "three";
 
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-//import { shortestPath, start, finish } from "ko/navigation.html"
+import { shortestPath, start, finish } from "ko/navigation.html"
 
 let container, camera, scene, renderer;
 
